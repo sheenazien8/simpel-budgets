cdce68c8aea9f7d19eb0117cd42c112e88c7f88d
sheenazien8 <8chelsealampard>
2023-02-04 06:47:08 +0700

    add d-h-container

--- resources/js/Components/Layout.tsx
+++ resources/js/Components/Layout.tsx
@@ -104,47 +104,6 @@ const Layout = (props: ILayout) => {
                         <span className="sr-only">View notifications</span>
                         <BellIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
-
-                      {/* Profile dropdown */}
-                      {/* <Menu as="div" className="relative ml-3">
-          <div>
-          <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          <span className="sr-only">Open user menu</span>
-          <img
-          className="h-8 w-8 rounded-full"
-          src={user.imageUrl}
-          alt=""
-          />
-          </Menu.Button>
-          </div>
-          <Transition
-          as={Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-          >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          {userNavigation.map((item) => (
-          <Menu.Item key={item.name}>
-          {({ active }) => (
-          <a
-          href={item.href}
-          className={classNames(
-          active ? "bg-gray-100" : "",
-          "block px-4 py-2 text-sm text-gray-700",
-          )}
-          >
-          {item.name}
-          </a>
-          )}
-          </Menu.Item>
-          ))}
-          </Menu.Items>
-          </Transition>
-          </Menu> */}
                     </div>
                     <div className="-mr-2 flex items-center sm:hidden">
                       {/* Mobile menu button */}
@@ -234,7 +193,7 @@ const Layout = (props: ILayout) => {
           </Disclosure>
         </div>
 
-        <div className="py-10">
+        <div className="py-10 d-h-container">
           <header className="mt-10">
             {(info as MInfo)?.month ||
             info?.budget ||
