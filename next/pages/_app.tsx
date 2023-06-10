dc869c211310a44b2d66c134abbff7152d2a2e29
zien <125542294+sheenabvarta@users.noreply.github.com>
2023-06-10 20:06:16 +0700

    add index page and add favicon

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Simpel Budget || Open source made with 🖤</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
