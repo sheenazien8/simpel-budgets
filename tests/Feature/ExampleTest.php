3be88dc30fd7a8efbc9e3a2f3c1495cba8e5eba8
sheenazien8 <sheena.geekgarden@bvarta@gmail.com>
2023-09-03 07:37:18 +0700

    fix(pipeline): fixing unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/test');
 
         $response->assertStatus(200);
     }
